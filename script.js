fbdc6bdd3cfdc19ab1c6f640e771c01ed05a9697
Leighton Grant <oneal.grant@outlook.com>
2023-01-01 22:48:35 +0000

    completes getPasswordOptions function and adds some comments

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 import { specialCharacters, numericCharacters, lowerCasedCharacters, upperCasedCharacters } from "./modules/chars.js";
 
-// Hard coded options for testing
-let passwordLength = 16;
-let options = [[upperCasedCharacters, true], [specialCharacters, true], [numericCharacters, true]];
-let selectedCharacters = lowerCasedCharacters;
-options.forEach((opt) => {
-    if (opt[1] === true) {
-        selectedCharacters = selectedCharacters.concat(opt[0]);
-    }
-});
 
 // Function to prompt user for password options
 const getPasswordOptions = () => {
+    const passwordLength = document.getElementById('passwordlength').value;
     const upperCaseChecked = document.getElementById('uppercasechars').checked;
+    const numericChecked = document.getElementById('numericchars').checked;
+    const specialCharactersChecked = document.getElementById('specialchars').checked;
+    const options = [[upperCasedCharacters, upperCaseChecked], [numericCharacters, numericChecked], [specialCharacters, specialCharactersChecked]];
+    // Default option
+    let selectedCharacters = lowerCasedCharacters;
+    // Loop through options and create an array of all selected characters
+    options.forEach((opt) => {
+        if (opt[1] === true) {
+            selectedCharacters = selectedCharacters.concat(opt[0]);
+        }
+    });
+    // Return an array of password length and all selected characters
+    return [passwordLength, selectedCharacters];
 }
 
 // Function for getting a random element from an array
@@ -24,9 +29,14 @@ const getRandom = (arr) => {
 // Function to generate password with user input
 const generatePassword = (arr) => {
     let password = '';
+    // Get password length for password options
+    const passwordLength = getPasswordOptions[0];
+    // Loop the length of the password, select a random 
+    // character from array and concatnate to password variable
     for (let i = 0; i < passwordLength; i++) {
         password += getRandom(arr);
     }
+
     return password;
 }
 
@@ -38,7 +48,7 @@ const writePassword = () => {
     const password = generatePassword(selectedCharacters);
     const passwordText = document.querySelector('#password');
     passwordText.value = password;
-    console.log(ucase.checked);
+    console.log(getPasswordOptions());
 }
 
 // Add event listener to generate button
