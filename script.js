844f0570e9cfa12d7f2bd7a5ea02a9a943ae99e5
Leighton Grant <oneal.grant@outlook.com>
2023-01-01 21:20:40 +0000

    adds hard coded options for testing

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 import { specialCharacters, numericCharacters, lowerCasedCharacters, upperCasedCharacters } from "./modules/chars.js";
 
+// Hard coded options for testing
+let passwordLength = 16;
+let options = [[upperCasedCharacters, true], [specialCharacters, true], [numericCharacters, true]];
+let selectedCharacters = lowerCasedCharacters;
+options.forEach((opt) => {
+    if (opt[1] === true) {
+        selectedCharacters = selectedCharacters.concat(opt[0]);
+    }
+});
+
 // Function to prompt user for password options
 const getPasswordOptions = () => {
-
 }
 
 // Function for getting a random element from an array
@@ -10,11 +19,11 @@ const getRandom = (arr) => {
     let randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
 }
-console.log(getRandom(specialCharacters));
+// console.log(getRandom(specialCharacters));
 
 // Function to generate password with user input
 const generatePassword = () => {
-    return "onadpfh91234429";
+
 }
 
 // Get references to the #generate element
