926e923496272542431772d83b0f7da7703b96af
Leighton Grant <oneal.grant@outlook.com>
2023-07-21 22:46:32 +0100

    Adds function to generate characters

--- modules/chars.js
+++ modules/chars.js
@@ -1,91 +1,21 @@
+// Function to get Characters
+function getChars(start, end) {
+  const chars = [];
+  for (let i = start; i < end; i++) {
+    chars.push(String.fromCharCode(i));
+  }
+  return chars;
+}
 // Array of special characters to be included in password
-const specialCharacters = [
-    '@',
-    '%',
-    '+',
-    '\\',
-    '/',
-    "'",
-    '!',
-    '#',
-    '$',
-    '^',
-    '?',
-    ':',
-    ',',
-    ')',
-    '(',
-    '}',
-    '{',
-    ']',
-    '[',
-    '~',
-    '-',
-    '_',
-    '.'
-];
+const specialCharacters = [...getChars(33, 47), ...getChars(59, 64), ...getChars(91, 96)];
 
 // Array of numeric characters to be included in password
-const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numericCharacters = getChars(48, 58);
 
 // Array of lowercase characters to be included in password
-const lowerCasedCharacters = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-];
+const lowerCasedCharacters = getChars(97, 122);
 
 // Array of uppercase characters to be included in password
-const upperCasedCharacters = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-];
+const upperCasedCharacters = getChars(65, 90);
 
 export { specialCharacters, numericCharacters, lowerCasedCharacters, upperCasedCharacters };
